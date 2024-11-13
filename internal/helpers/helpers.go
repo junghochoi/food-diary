34a852b1ebb1182e58a7df372e6d5d37b9546e42
dchoi315 <dchoi315@umd.edu>
2024-11-12 21:12:41 -0500

    Disallow unknown fields

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -20,11 +20,10 @@ func RespondWithError(w http.ResponseWriter, statusCode int, message string, err
 }
 
 func ReadJson(w http.ResponseWriter, req *http.Request, res interface{}) error {
-	err := json.NewDecoder(req.Body).Decode(res)
+	decoder := json.NewDecoder(req.Body)
+	decoder.DisallowUnknownFields()
 
-	if err == nil {
-		return nil
-	}
+	err := decoder.Decode(res)
 
 	var syntaxError *json.SyntaxError
 	var unmarshalTypeError *json.UnmarshalTypeError
@@ -46,12 +45,14 @@ func ReadJson(w http.ResponseWriter, req *http.Request, res interface{}) error {
 			"body contains incorrect JSON type (at character %d)",
 			unmarshalTypeError.Offset,
 		)
-	// An io.EOF error will be returned by Decode() if the request body is empty. We // check for this with errors.Is() and return a plain-english error message
+	// An io.EOF error will be returned by Decode() if the request body is empty. We
+	// check for this with errors.Is() and return a plain-english error message
 	// instead.
 	case errors.Is(err, io.EOF):
 		return errors.New("body must not be empty")
 	// A json.InvalidUnmarshalError error will be returned if we pass a non-nil
-	// pointer to Decode(). We catch this and panic, rather than returning an error // to our handler. At the end of this chapter we'll talk about panicking
+	// pointer to Decode(). We catch this and panic, rather than returning an error
+	// to our handler. At the end of this chapter we'll talk about panicking
 	// versus returning errors, and discuss why it's an appropriate thing to do in // this specific situation.
 	case errors.As(err, &invalidUnmarshalError):
 		panic(err)
